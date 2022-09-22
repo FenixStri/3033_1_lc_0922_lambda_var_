5b7404fbdfa96d5a313e79e45683ce4d8af89fee
Joe Jennings <lane@lanestrickland.com>
2022-09-22 16:12:54 -0500

    Add project files.

---
+++ 3033_1_lc_0922_lambda_var_/Program.cs
@@ -0,0 +1,74 @@
+﻿// Fenix Strickland 113437176
+
+//lambda
+
+List<double> gradesList;
+gradesList = new List<double>();
+gradesList.Add(96.6);
+gradesList.Add(95.6);
+gradesList.Add(94.5);
+gradesList.Add(93.4);
+gradesList.Add(92.3);
+gradesList.Add(91.2);
+
+//want to sort from min-max ascending
+gradesList.Sort((x,y) => 
+{
+    if (x > y)
+    {
+        return 1;
+    }
+    else if (x == y)
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+    
+
+}); //1, -1, 0
+
+Console.WriteLine("Sorted list");
+for (int i = 0;i<gradesList.Count ;i++ ) 
+{
+    Console.WriteLine(gradesList[i]);
+}
+
+double highestGradeDbl = gradesList[0];
+for(int i = 1; i<gradesList.Count; i++)
+{
+    if (gradesList[i] > highestGradeDbl)
+    {
+        highestGradeDbl = gradesList[i];
+    }
+}
+
+Console.WriteLine(highestGradeDbl);
+
+//highestGradeDbl = gradesList.Max( x => x.weight); use x.weight if student has many attributes and you want to specify which max you want
+//Console.WriteLine(highestGradeDbl);
+
+List<double> gradesList2;
+gradesList2 = gradesList; // now these two cards have access to the same room . there is still only one list
+
+
+
+Console.ReadLine();
+//lambda expression 
+// short version of function
+//() => {}
+
+string testStr;
+testStr = new string("virtual properties behave like virtual methods, except for the differences in declaration and invocation syntax");
+//"xinglon"
+
+int l = testStr.Count((char x) => { return "abcd".Contains(x); });
+Console.WriteLine(l);
+
+
+
+
+
+Console.ReadLine();
---
+++ var/Program.cs
@@ -0,0 +1,46 @@
+﻿// Fenix Strickland 113437176
+//var
+
+string resultStr = StringExtensionFunction.FirstPlusLastLetter("xinglong ju");
+Console.WriteLine(resultStr);
+
+resultStr = "aiden".FirstPlusLastLetter();
+Console.WriteLine(resultStr);
+
+var p1 = new {name = "xinglong", age = 30, memid = "jksflkm9043" };
+Console.WriteLine(p1.memid);
+
+
+Student stu1;
+stu1 = new Student() { name = "Fenix", age = 20, grade = 99.9};
+
+
+
+
+string nameStr = "xinglong ju";
+var nameStr2 = nameStr;
+
+var var1 = 2;
+
+
+
+
+Console.ReadLine();
+
+
+public class Student 
+{
+    public int age { get; set; }
+    public string name { get; set; }
+    public double grade { get; set; }
+}
+
+public static class StringExtensionFunction 
+{
+    public static string FirstPlusLastLetter(this string str)
+    {
+        return string.Format($"{str[0]}{str[str.Length-1]}");
+    }
+
+
+}
